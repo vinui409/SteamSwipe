f46914555b6f07a1568d357128c3be12d0d41eb4
Vinu Ihalagamage <vinu.i2017@gmail.com>
2024-07-12 12:09:27 -0700

    db anon key and URL

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,9 +1,7 @@
-// src/supabaseClient.js
-
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://xyzcompany.supabase.co'; // Replace with your Supabase URL
-const supabaseAnonKey = 'your-anon-key'; // Replace with your Supabase anon key
+const supabaseUrl = 'https://nrtazkpzgpqsbymhmfdj.supabase.co'; 
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ydGF6a3B6Z3Bxc2J5bWhtZmRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA4MDk5MzAsImV4cCI6MjAzNjM4NTkzMH0.Hy2aXjmdXokjRlBEFqItu-wlPOnGsixLzuYdiS6TW9E'; 
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
